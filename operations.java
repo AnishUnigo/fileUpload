3587a14c7587b500b93c7a5042cad81db8160fb8
Anish <anish1258@gmail.com>
2018-01-07 14:32:37 -0600

    first commit on operations.java file

---
+++ operations.java
@@ -0,0 +1,43 @@
+/*  Volume in drive C has no label.
+ Volume Serial Number is BE26-8A59
+
+ Directory of C:\Users\Anish\Desktop\GitHub
+
+01/07/2018  02:24 PM    <DIR>          .
+01/07/2018  02:24 PM    <DIR>          ..
+07/13/2017  03:01 PM    <DIR>          Anand
+07/13/2017  03:09 PM    <DIR>          anand1
+07/13/2017  03:01 PM    <DIR>          anish
+07/13/2017  11:54 AM                11 Anish.txt
+07/13/2017  12:05 PM    <DIR>          GitHubTester
+07/13/2017  11:49 AM                12 helloWorld.txt
+01/07/2018  02:24 PM                 0 operations.java
+07/14/2017  12:10 AM    <DIR>          th
+               3 File(s)             23 bytes
+               7 Dir(s)  104,121,102,336 bytes free
+ */
+ 
+ public class operations{
+	 
+	 private int a;
+	 private int b;
+	 
+	 public int addition(int a, int b){
+		 
+		 return a+b;
+	 }
+	 
+	 public int substration(int a, int b){
+		 
+		 return a-b;
+	 }
+	 public int multiplication(int a, int b){
+		 
+		 return a*b;
+	 }
+	 public int division(int a, int b){
+		 
+		 return a/b;
+	 }
+	 
+ }
